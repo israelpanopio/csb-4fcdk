8f90afb13466e961426d04ea7b573741f1974eb6
Code Sandbox <hello@codesandbox.io>
2021-11-23 14:34:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e4b4b7b0dca62cc2a82e4c44b0d15af6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c880fa9636c43b6e739f69e237231240",
+    "url": "https://israelpanopio.github.io/csb-4fcdk/index.html"
+  },
+  {
+    "revision": "9ead95e16aae1e438d17",
+    "url": "https://israelpanopio.github.io/csb-4fcdk/static/js/2.5783afce.chunk.js"
+  },
+  {
+    "revision": "2cb6183201d45b963f2a",
+    "url": "https://israelpanopio.github.io/csb-4fcdk/static/js/main.bd932257.chunk.js"
+  },
+  {
+    "revision": "7156b9794900c4dd4863",
+    "url": "https://israelpanopio.github.io/csb-4fcdk/static/js/runtime-main.f2f5aa52.js"
+  }
+]);
